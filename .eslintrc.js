25ed8423074beb6b154aef10b0578ca1f9a1dd51
Mario Tacke <tacke.mario@gmail.com>
2020-11-30 08:52:42 -0800

    chore: modernize repository

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true,
-    "mocha": true
+  'env': {
+    'es6': true,
+    'node': true,
+    'mocha': true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "sourceType": "module"
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'sourceType': 'module',
   },
-  "rules": {
-    "indent": ["error", 2, { "SwitchCase": 1 }],
-    "quotes": [2, "single"],
-    "semi": 2
-  }
+  'rules': {
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'quotes': [2, 'single'],
+    'semi': 2,
+  },
 };
